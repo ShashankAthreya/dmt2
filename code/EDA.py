55cce0c8a0b8c79c0c5fbc5b4a8d3e0c7ef99f88
smrutii07 <94400783+smrutii07@users.noreply.github.com>
2022-05-20 12:54:50 +0200

    Update EDA.py

--- code/EDA.py
+++ code/EDA.py
@@ -16,3 +16,22 @@ country_id = booked['prop_country_id']
 country_id.value_counts()
 
 sns.countplot('prop_country_id',data=booked.sort_values(by=['prop_country_id']))
+
+
+
+## check stats for max booked hotel destination 
+
+max_prop_booked = 219 
+
+country_customers = booked[booked["prop_country_id"] == max_prop_booked]
+
+country_customers["visitor_location_country_id"].value_counts().plot(kind='pie',autopct='%1.1f%%')
+
+## stats for independent hotels and chains 
+
+chain_ind = booked['prop_brand_bool']
+count_row = booked.shape[0]
+print(count_row)
+chain_ind.value_counts().plot(kind='pie',autopct='%1.1f%%')
+chain_ind.value_counts()
+
